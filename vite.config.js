bd31c055706b79942870b4111d6cdc0b0511a79c
Lionel Armanet <lionel.armanet@gmail.com>
2025-09-15 08:38:47 +0200

    feat: index page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        resume: resolve(__dirname, 'resume.html'),
+      },
+    },
+  },
 })
